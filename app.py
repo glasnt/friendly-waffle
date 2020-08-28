3a08aedab343e49ad60cdc5a3d07aaa70ff87eac
Katie McLaughlin <katie@glasnt.com>
2020-08-28 14:25:51 +1000

    Black

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ import os
 from datetime import datetime
 
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def hello_world():
     data = {"time": str(datetime.now())}
     return render_template("index.html", data=data)
 
+
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
